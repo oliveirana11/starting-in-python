b33771512544a90c4b00204e0458d521e4c48b01
Ana Oliveira <anakarlaoliveira@gmail.com>
2026-07-13 21:48:23 -0300

    Uncomment code and fix formatting in aula3_range_pandas.py

--- aula3_range_pandas.py
+++ aula3_range_pandas.py
@@ -1,18 +1,18 @@
-# 25/5
 # for
 # len quantidade de coisas dentro de uma lista
 # for variavel in conjunto - print (variavel) --> no for a variavel assume todas as coisas dentro do conjunto
-#professores = ['Danilo', 'Gabi', 'Acelote', 'Toniho', 'Leninha']
-#disciplinas = ['python', 'historia', 'matemática', 'quimica', 'biologia','portugues']
-#print(len(professores))
-#print(len(disciplinas))
 
-#print('Os professores são: ')
-#for nome in professores:
-#    print(nome)
-#print('As matérias que ensinam são: ')
-#for materia in disciplinas:
-#    print(materia)
+professores = ['Danilo', 'Gabi', 'Acelote', 'Toniho', 'Leninha']
+disciplinas = ['python', 'historia', 'matemática', 'quimica', 'biologia','portugues']
+print(len(professores))
+print(len(disciplinas))
+
+print('Os professores são: ')
+for nome in professores:
+    print(nome)
+print('As matérias que ensinam são: ')
+for materia in disciplinas:
+    print(materia)
 
 # Fizemos for por elementos, mas gostariamos de mostrar na tela
 # o professor x materia. Para isso precisamos parear elemento por posição
@@ -21,22 +21,22 @@
 # ​A função *range* gera uma sequência de números, 
 # e o *i* assume o valor de cada um deles a cada volta
 
-#for i in range(len(professores)):
-#    print(f'professores[{i}] = {professores[i]}')
+for i in range(len(professores)):
+   print(f'professores[{i}] = {professores[i]}')
 
-#for i in range(len(professores)):
-#    print(f'O/A professor/a {professores[i]} ensina {disciplinas[i]}')
+for i in range(len(professores)):
+    print(f'O/A professor/a {professores[i]} ensina {disciplinas[i]}')
 
-'''import pandas as pd
+import pandas as pd
 professores = ['Danilo', 'Gabi', 'Acelote', 'Toniho', 'Leninha', 'leo']
 disciplinas = ['python', 'historia', 'matemática', 'quimica', 'biologia','portugues']
 dados={
     'profs' : professores,
     'materias' : disciplinas
 }
-print(pd.DataFrame(dados))'''
+print(pd.DataFrame(dados))
 
-'''import pandas as pd
+import pandas as pd
 carros ={
     'modelo':['uno','gol','polo','kombi'],
     'preço':[20000, 30000, 60000, 15000],
@@ -45,4 +45,4 @@ carros ={
 }
 df = pd.DataFrame(carros)
 print(df)
-df.to_excel('carros.xlsx')'''
+df.to_excel('carros.xlsx')
