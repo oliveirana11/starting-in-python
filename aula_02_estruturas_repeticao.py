8089f71b008e30625870276ec611f3cebf481f2b
Ana Oliveira <anakarlaoliveira@gmail.com>
2026-07-13 21:45:31 -0300

    Fix input prompts and clean up code

--- aula_02_estruturas_repeticao.py
+++ aula_02_estruturas_repeticao.py
@@ -1,6 +1,5 @@
-    #18/5
     # While
-'''idoso = input('Você é idoso?\n(sim/não): ')
+idoso = input('Você é idoso?\n(sim/não): ')
 while idoso.lower() != 'sim' and idoso.lower() != 'não': 
     #.lower transforma o que o usuário digitou em minúsculo
     print("Você deve digitar sim ou não!!")
@@ -14,22 +13,22 @@ while cartao.lower() != 'sim' and cartao.lower() != 'não':
 if idoso == 'sim' and cartao == 'sim':
     print('Pode estacionar!')
 else:
-    print('Procure outra vaga!')'''
+    print('Procure outra vaga!')
 
-'''letra = input('Digite uma letra: ')
+letra = input('Digite uma letra: ')
 if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
     print('Você digitou uma vogal!')
 else:
-    print('Não é uma vogal!')'''
+    print('Não é uma vogal!')
 
-'''vogais = ['a', 'e', 'i', 'o', 'u']
+vogais = ['a', 'e', 'i', 'o', 'u']
 letra = input('Digite uma letra: ')
 if letra in vogais:
     print(f'{letra} é uma vogal')
 else:
-    print(f'{letra} não é uma vogal')'''
+    print(f'{letra} não é uma vogal')
 
-'''estado_civil = input('VocÊ é solteiro/a(s),casado/a(c), divorciado/a(d), viúvo/a(v)\n: ')
+estado_civil = input('VocÊ é solteiro/a(s),casado/a(c), divorciado/a(d), viúvo/a(v)\n: ')
 while estado_civil not in ['s', 'c', 'v', 'd']:
     print('Erro!')
-    estado_civil = input('VocÊ é solteiro/a(s),casado/a(c), divorciado/a(d), viúvo/a(v)\n: ')'''
+    estado_civil = input('VocÊ é solteiro/a(s),casado/a(c), divorciado/a(d), viúvo/a(v)\n: ')
